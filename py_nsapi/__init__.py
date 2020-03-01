fbeb9cd3de837e2e5f966847d52ba2c650862e0c
Bas Nijholt <basnijholt@gmail.com>
2020-03-01 14:56:04 +0100

    use black, isort, and pyupgrade

--- py_nsapi/__init__.py
+++ py_nsapi/__init__.py
@@ -1,5 +1,5 @@
-from .stationslijst import stations
-from .storingen import storingen
 from .prijzen import prijzen
 from .reisadviezen import reisadviezen
+from .stationslijst import stations
+from .storingen import storingen
 from .vertrektijden import vertrektijden
--- py_nsapi/prijzen.py
+++ py_nsapi/prijzen.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-# -*- encoding: utf-8 -*-
 """
 Voor gebruik van de webservice is aparte autorisatie vereist. 
 Deze autorisatie wordt verleend na ontvangst van een getekend contract. 
 Dit contract is op te vragen via nsr.api@ns.nl.
 """
 
-import requests
+import logging
+import os
 import sys
 import time
-import os
-import logging
+
+import requests
+
 import xmltodict
 
 from .trains import Trains
+
 
 class prijzen(Trains):
     """
@@ -23,16 +25,24 @@ class prijzen(Trains):
     def getData(self, fromST=None, toST=None, viaST="", dateTime=""):
         try:
             if fromST is None or toST is None:
-                raise Exception("You must provide a From and To station")    
-            
-            #fetch the elements from the NS API
-            url = "http://webservices.ns.nl/ns-api-prijzen-v3?from={}&to={}&via={}&dateTime={}".format(fromST,toST,viaST,dateTime)
+                raise Exception("You must provide a From and To station")
+
+            # fetch the elements from the NS API
+            url = f"http://webservices.ns.nl/ns-api-prijzen-v3?from={fromST}&to={toST}&via={viaST}&dateTime={dateTime}"
 
             root = self.goFetch(url)
-            
+
             return xmltodict.parse(root, dict_constructor=dict)
-            
+
         except Exception as e:
             exc_type, exc_obj, exc_tb = sys.exc_info()
             fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-            logging.warning(str(e) + " | " + str(exc_type) + " | " + str(fname) + " | " + str(exc_tb.tb_lineno))  
+            logging.warning(
+                str(e)
+                + " | "
+                + str(exc_type)
+                + " | "
+                + str(fname)
+                + " | "
+                + str(exc_tb.tb_lineno)
+            )
